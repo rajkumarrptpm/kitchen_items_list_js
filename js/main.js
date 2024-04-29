8e14a6076a8bbd3954ddbeeef19bc153377cff4b
Raj kumar R <78129713+rajkumarrptpm@users.noreply.github.com>
2024-04-29 15:20:53 +0530

    set=and-get-items-to-local-storage

--- js/main.js
+++ js/main.js
@@ -3,38 +3,76 @@ let addBtn = document.getElementById("add-btn");
 let kitchenItemsList = document.getElementById("kitchen-items-list");
 // create an empty array
 let data=[];
+let itemData;
+
+function buildUI(){
+
+    kitchenItemsList.textContent="";
+    data.forEach((item)=>{
+        console.log(item);
+        let li = document.createElement("li");
+        let spanEl = document.createElement("span");
+        spanEl.classList.add("kitchenItem");
+        li.appendChild(spanEl);
+        
+        spanEl.innerText=item
+        // li.innerText = itemData;
+        li.style.cssText = "animation-name:slideIn;";
+        kitchenItemsList.appendChild(li);
+        kitchenInput.value="";
+        kitchenInput.focus();
+
+
+        // create update button
+        let editBtn = document.createElement('i');
+        editBtn.classList.add('fas','fa-edit');
+        li.appendChild(editBtn);
+        
+
+        // create delete button
+        let trashBtn = document.createElement('i');
+        trashBtn.classList.add('fas','fa-trash');
+        li.appendChild(trashBtn);
+    })
+
+    
+
+
+
+    
+}
+
+
+function setLocalStorage(){
+    localStorage.setItem("item",JSON.stringify(data));
+}
+
+function getLocalStorage(){
+   
+    if (localStorage.getItem("item")){
+        data = JSON.parse(localStorage.getItem("item"));
+        buildUI();
+    }
+    
+}
+
 
 // step 2 :- Add kitchen Items
 function addKitchenItems(){
-    let itemData = kitchenInput.value;
+    itemData = kitchenInput.value;
+    
     data.push(itemData);
+    // set to localstorage
+    setLocalStorage()
+    
+
+    // get from localstorage
+    getLocalStorage()
+
+
+
+
     // create dom elements
-    let li = document.createElement("li");
-    let spanEl = document.createElement("span");
-    spanEl.classList.add("kitchenItem")
-    li.appendChild(spanEl);
-    
-    spanEl.innerText=itemData
-    // li.innerText = itemData;
-    li.style.cssText = "animation-name:slideIn;"
-    kitchenItemsList.appendChild(li);
-    kitchenInput.value="";
-    kitchenInput.focus();
-
-
-    // create update button
-    let editBtn = document.createElement('i');
-    editBtn.classList.add('fas','fa-edit');
-    li.appendChild(editBtn)
-    
-
-    // create delete button
-    let trashBtn = document.createElement('i');
-    trashBtn.classList.add('fas','fa-trash');
-    li.appendChild(trashBtn);
-
-
-
     
 
 }
@@ -70,6 +108,18 @@ function editKitchenItem(event){
 
 // step 1 :- Add an event listner to the button
 addBtn.addEventListener('click',addKitchenItems);
-kitchenItemsList.addEventListener('click',deleteKitchenItem)
-kitchenItemsList.addEventListener('click',editKitchenItem)
-          
+kitchenItemsList.addEventListener('click',deleteKitchenItem);
+kitchenItemsList.addEventListener('click',editKitchenItem);
+getLocalStorage();
+
+
+// local storage
+
+
+// set data to local storage
+// localStorage.setItem("name","Rajkumar");   
+
+
+// get data from local storage
+
+// let s = localStorage.getItem("name");
