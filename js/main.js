f7b4bd6c075e9b69e3feacdce7218e6a9783f15e
Raj kumar R <78129713+rajkumarrptpm@users.noreply.github.com>
2024-04-29 08:15:03 +0530

    add-update-button-updation-successful

--- js/main.js
+++ js/main.js
@@ -10,17 +10,32 @@ function addKitchenItems(){
     data.push(itemData);
     // create dom elements
     let li = document.createElement("li");
-    li.innerText = itemData;
+    let spanEl = document.createElement("span");
+    spanEl.classList.add("kitchenItem")
+    li.appendChild(spanEl);
+    
+    spanEl.innerText=itemData
+    // li.innerText = itemData;
     li.style.cssText = "animation-name:slideIn;"
     kitchenItemsList.appendChild(li);
     kitchenInput.value="";
     kitchenInput.focus();
+
+
+    // create update button
+    let editBtn = document.createElement('i');
+    editBtn.classList.add('fas','fa-edit');
+    li.appendChild(editBtn)
     
 
-    // create trash button
+    // create delete button
     let trashBtn = document.createElement('i');
     trashBtn.classList.add('fas','fa-trash');
     li.appendChild(trashBtn);
+
+
+
+    
 
 }
 
@@ -28,7 +43,26 @@ function addKitchenItems(){
 function deleteKitchenItem(event){
     if (event.target.classList[1] === 'fa-trash'){
         let item = event.target.parentElement;
-        item.remove();
+        item.classList.add('slideOut');
+        item.addEventListener('transitionend',function(){
+            // console.log("animation has been completed");
+            item.remove();
+        });
+        // item.remove();
+    }
+}
+
+
+// function for edit kitchen item
+function editKitchenItem(event){
+    if (event.target.classList[1] === 'fa-edit'){
+        let editedValue = prompt("update the item ");
+        console.log(event);
+        let item = event.target.parentElement;
+        // event.target.parentElement.firstChild.textContent=editedValue ;
+        let spanItem = item.querySelector("span");
+        spanItem.innerText = editedValue;
+        
     }
 }
 
@@ -37,3 +71,5 @@ function deleteKitchenItem(event){
 // step 1 :- Add an event listner to the button
 addBtn.addEventListener('click',addKitchenItems);
 kitchenItemsList.addEventListener('click',deleteKitchenItem)
+kitchenItemsList.addEventListener('click',editKitchenItem)
+          
